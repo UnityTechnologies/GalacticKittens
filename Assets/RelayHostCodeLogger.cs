a734bf5ad3f64f4077d3a199650c83cf05880c5f
Esteban Maldonado <esteban.maldonado@unity3D.com>
2022-05-25 15:54:21 -0700

    to test host

---
+++ Assets/RelayHostCodeLogger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelayHostCodeLogger : MonoBehaviour
+{
+    private float m_currentTime = 0f;
+
+    // Update is called once per frame
+    async void Update()
+    {
+        m_currentTime += Time.deltaTime;
+
+        if (m_currentTime >= 5f)
+        {
+            m_currentTime = 0f;
+
+            var joinCodeAsyncTask = RelayManager.Instance.JoinCodeAsyncTask;
+
+            if (joinCodeAsyncTask != null)
+            {
+                string joinCode = await joinCodeAsyncTask;
+
+                Debug.Log($"Host Join Code: {joinCode}");
+            }
+        }
+    }
+}
